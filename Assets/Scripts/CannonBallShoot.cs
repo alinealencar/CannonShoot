f9dec450a0ce80bb7ed8dc43b9edc8beb6a0d750
Aline <aline.nevesalencar@georgebrown.ca>
2018-01-18 21:05:53 -0500

    Add animation to player

--- Assets/Scripts/CannonBallShoot.cs
+++ Assets/Scripts/CannonBallShoot.cs
@@ -12,16 +12,24 @@ public class CannonBallShoot : MonoBehaviour {
 
 	Rigidbody2D _rb;
 	Transform _transform;
+	private Animator _animator = null;
 
 	// Use this for initialization
 	void Start () {
+		_animator = gameObject.GetComponent<Animator> ();
 		_transform = gameObject.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.F))
-			FireCannonBall ();
+		if (Input.GetKeyDown (KeyCode.F)) {
+			//Set the trigger to initiate the animation (check animator in the "fire" state)
+			_animator.SetTrigger ("fire");
+			//The cannonball can be shot from here, but there is a small delay
+			//in between the animation and the actual shot. So, we will call this
+			//method from the animator
+			//FireCannonBall ();
+		}
 	}
 
 	public void FireCannonBall(){
@@ -32,6 +40,7 @@ public class CannonBallShoot : MonoBehaviour {
 		
 		_rb = p.GetComponent<Rigidbody2D> ();
 
+		//This is just so the cannon doesnt move back when it shoots
 		Physics2D.IgnoreCollision (
 			gameObject.GetComponent<Collider2D> (),
 			p.GetComponent<Collider2D> ());
